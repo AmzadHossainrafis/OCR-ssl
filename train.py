d344053ac8388887a4719cb0e1327190455b7291
Amzad hossain <amzad.rafi@northsouth.edu>
2022-06-02 03:44:49 +0600

    training pipeline is set (not done)

---
+++ train.py
@@ -0,0 +1,21 @@
+from gc import callbacks
+from tkinter import S
+from dataloader import Dataloader
+import tensorflow as tf
+import numpy as np
+from utils import *
+from model import *
+
+config=read_yaml("config.yaml")
+model=build_model()
+callbacks=SelectCallbacks()
+
+#train dataloader 
+train_ds=Dataloader(config['batch_size'],train_data)
+
+#valloader
+val_ds=Dataloader(config['batch_size'],val_data)
+
+history=model.fit(train_ds, validation_data=val_ds, epochs=config['epochs'], callbacks=callbacks)
+
+
