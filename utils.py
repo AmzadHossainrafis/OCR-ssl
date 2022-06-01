f3a59417399bc5c409af00384dd215ca0fe7b11e
Amzad hossain <amzad.rafi@northsouth.edu>
2022-06-01 23:20:13 +0600

    chang in read_img_lavles function

--- utils.py
+++ utils.py
@@ -13,8 +13,10 @@ from tensorflow.keras import layers
 def read_yaml(path='config.yaml'):
     """
     Reads the yaml file and returns the data in a dictionary.
-    :param path: The path to the yaml file.
-    :return: The data in the yaml file.
+    args: 
+    path(str) The path to the yaml file.
+    return: The data in the yaml file.
+
     """
     with open(path, 'r') as stream:
         data_loaded = yaml.load(stream, Loader=yaml.FullLoader)
@@ -25,8 +27,10 @@ def read_yaml(path='config.yaml'):
 def read_img_lables(path):
     """
     Reads the images and labels from the given path.
-    :param path: The path to the images and labels.
-    :return: The images and labels.
+    args: path(str) The path to the images and labels.
+
+    return: The images and labels and unique labels .
+
     """
     data_dir = Path("./data/captcha_images_v2/")
     images_dir = [map(str, list(data_dir.glob("*.png")))]
@@ -34,14 +38,21 @@ def read_img_lables(path):
     for i in os.listdir(path):
         m=i.split(".png")[0]
         labels.append(m)
-    return images_dir, labels
+    characters = set(char for label in labels for char in label)
+    return images_dir, labels,characters
 
            
 
 #ocs model data download
 def split_data(images, labels, train_size=0.9, shuffle=True):
     """ Split data into training and test sets.
-    
+    args:
+
+    images:(list) list of images dir 
+    labels:(list) list of labels 
+    train_size:(float) percentage of data to use for training
+    shuffle: bool, whether to shuffle the data before splitting
+
     """
 
     # 1. Get the total size of the dataset
@@ -67,8 +78,7 @@ def data_preprocess(images, labels,characters):
     """
 
     char_to_num = layers.StringLookup(
-    vocabulary=list(characters), mask_token=None
-)
+    vocabulary=list(characters), mask_token=None)
     # 1. Read image
     img=cv2.imread(images,cv2.IMREAD_GRAYSCALE)
     # 2. Resize image
@@ -80,29 +90,9 @@ def data_preprocess(images, labels,characters):
     # 4. Normalize image
     img=img/255
     # transpose image
-    img=img.T
+    img=np.transpose(img,(2,0,1))
     label=tf.strings.unicode_split(labels, input_encoding="UTF-8")
     char_to_num(label)
     return {"image": img, "label": label}
 
 
-
-
-
-
-
-    # img = tf.io.read_file(img_path)
-    # # 2. Decode and convert to grayscale
-    # img = tf.io.decode_png(img, channels=1)
-    # # 3. Convert to float32 in [0, 1] range
-    # img = tf.image.convert_image_dtype(img, tf.float32)
-    # # 4. Resize to the desired size
-    # img = tf.image.resize(img, [img_height, img_width])
-    # # 5. Transpose the image because we want the time
-    # # dimension to correspond to the width of the image.
-    # img = tf.transpose(img, perm=[1, 0, 2])
-    # # 6. Map the characters in label to numbers
-    # label = char_to_num(tf.strings.unicode_split(label, input_encoding="UTF-8"))
-    # # 7. Return a dict as our model is expecting two inputs
-    # 
- 
