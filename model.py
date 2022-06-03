c94e4e5a04ba80ca5a1754d53cce408f2d09e70b
Amzad hossain <amzad.rafi@northsouth.edu>
2022-06-03 21:11:36 +0600

    model modified according the last error

--- model.py
+++ model.py
@@ -1,49 +1,34 @@
 import tensorflow as tf
 from tensorflow import  keras
 from tensorflow.keras import layers
-from Ctc import CTCLayer,CTCLoss
-img_width=200
-img_height=50
+from Ctc import CTCLayer
+from utils import read_yaml
 
 
-
-
+config=read_yaml()
 def build_model():
     # Inputs to the model
     input_img = layers.Input(
-        shape=(img_width, img_height, 1), name="image", dtype="float32"
+        shape=(config["width"], config["height"], 1), name="image", dtype="float32"
     )
     labels = layers.Input(name="label", shape=(None,), dtype="float32")
 
     # First conv block
-    x = layers.Conv2D(
-        32,
-        (3, 3),
-        activation="relu",
-        kernel_initializer="he_normal",
-        padding="same",
-        name="Conv1",
-    )(input_img)
+    x = layers.Conv2D(32,(3, 3),activation=config["activation"],kernel_initializer=config["kernel_initializer"],padding=config["padding"],name="Conv1",)(input_img)
     x = layers.MaxPooling2D((2, 2), name="pool1")(x)
 
     # Second conv block
     x = layers.Conv2D(
-        64,
-        (3, 3),
-        activation="relu",
-        kernel_initializer="he_normal",
-        padding="same",
-        name="Conv2",
-    )(x)
+        64,(3, 3),activation=config["activation"],kernel_initializer=config["kernel_initializer"],padding=config["padding"],name="Conv2",)(x)
     x = layers.MaxPooling2D((2, 2), name="pool2")(x)
 
     # We have used two max pool with pool size and strides 2.
     # Hence, downsampled feature maps are 4x smaller. The number of
     # filters in the last layer is 64. Reshape accordingly before
     # passing the output to the RNN part of the model
-    new_shape = ((img_width // 4), (img_height // 4) * 64)
+    new_shape = ((config["width"] // 4), (config["height"] // 4) * 64)
     x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
-    x = layers.Dense(64, activation="relu", name="dense1")(x)
+    x = layers.Dense(64, activation=config['activation'], name="dense1")(x)
     x = layers.Dropout(0.2)(x)
 
     # RNNs
