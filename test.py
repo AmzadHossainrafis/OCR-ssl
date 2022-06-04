51649b28256abaac015855c1126358a79ee95663
Amzad hossain <amzad.rafi@northsouth.edu>
2022-06-04 14:48:33 +0600

    test is modified with new model

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
-from dataloader import DataLoader 
-#import load model from keras 
+from pickletools import optimize
 from train import valid_ds
 from keras.models import load_model
 from utils import read_yaml
-from model import build_model
+from Ctc import CTCLoss
 import tensorflow as tf
 
 
 
+
 config=read_yaml("config.yaml")
-model=build_model()
-#model=model.load_model(config["model_dir"])
-# bit confuse what to do .. i need to compile the model but i already compiled it in build_model()
-model.load_weights(config["model_dir"])
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=config["learning_rate"])
+model=load_model(config['model_dir'])
+model.compile('adam',loss=CTCLoss)
 model.evaluate(valid_ds)    
