362696f14f417637e6077ba4aa684540314db586
Amzad hossain <amzad.rafi@northsouth.edu>
2022-06-04 14:46:51 +0600

    model is reshaped for test

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
+from pickletools import optimize
 import tensorflow as tf
 from tensorflow import  keras
 from tensorflow.keras import layers
-from Ctc import CTCLayer
+from Ctc import CTCLayer,CTCLoss
 from utils import read_yaml
 
 
@@ -50,3 +51,44 @@ def build_model():
     # Compile the model and return
     model.compile(optimizer=opt)
     return model
+
+
+
+def build_model2():
+        # Inputs to the model
+    input_img = layers.Input(
+        shape=(config["width"], config["height"], 1), name="image", dtype="float32"
+    )
+    #labels = layers.Input(name="label", shape=(None,), dtype="float32")
+
+    # First conv block
+    x = layers.Conv2D(32,(3, 3),activation=config["activation"],kernel_initializer=config["kernel_initializer"],padding=config["padding"],name="Conv1",)(input_img)
+    x = layers.MaxPooling2D((2, 2), name="pool1")(x)
+
+    # Second conv block
+    x = layers.Conv2D(
+        64,(3, 3),activation=config["activation"],kernel_initializer=config["kernel_initializer"],padding=config["padding"],name="Conv2",)(x)
+    x = layers.MaxPooling2D((2, 2), name="pool2")(x)
+
+    # We have used two max pool with pool size and strides 2.
+    # Hence, downsampled feature maps are 4x smaller. The number of
+    # filters in the last layer is 64. Reshape accordingly before
+    # passing the output to the RNN part of the model
+    new_shape = ((config["width"] // 4), (config["height"] // 4) * 64)
+    x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
+    x = layers.Dense(64, activation=config['activation'], name="dense1")(x)
+    x = layers.Dropout(0.2)(x)
+
+    # RNNs
+    x = layers.Bidirectional(layers.LSTM(128, return_sequences=True, dropout=0.25))(x)
+    x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, dropout=0.25))(x)
+
+    # Output layer
+    output= layers.Dense(50, activation="softmax", name="dense2")(x)
+    model = keras.models.Model(
+        inputs=input_img, outputs=output, name="ocr_model_v1"
+    )
+    
+    return model 
+
+
